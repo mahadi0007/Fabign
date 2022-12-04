f770ba426ed2fceb23a7695757c7883d84df8a52
SirRatul <samsulratul98@gmail.com>
2022-12-05 01:19:40 +0600

    api url change to efg

--- utils/Http/hosturl.js
+++ utils/Http/hosturl.js
@@ -1,5 +1,5 @@
-// const HostUrl = "https://api.efgtailor.com";
-const HostUrl = "http://localhost:4010";
+const HostUrl = "https://api.efgtailor.com";
+// const HostUrl = "http://localhost:4010";
 const HostUrl2 = "http://localhost:4010";
 // export default { HostUrl, HostUrl2 };
 export { HostUrl, HostUrl2 };
