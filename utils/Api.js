4e3bc76205af9cedb5df3efcdfcb7c4e5baea3fc
SirRatul <samsulratul98@gmail.com>
2022-12-06 22:54:12 +0600

    api changes to fabign

--- utils/Api.js
+++ utils/Api.js
@@ -1,4 +1,4 @@
-export const API = "https://api.efgtailor.com/api/v1/e-efg";
+export const API = "http://api.fabign.com/api/v1/e-efg";
 // export const API = "http://localhost:4010/api/v1/e-efg";
-export const API2 = "https://api.efgtailor.com/api/v1";
+export const API2 = "http://api.fabign.com/api/v1";
 // export const API2 = "http://localhost:4010/api/v1";
